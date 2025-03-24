278d2bb279e0295e6d9edbbb40e54775e3e84012
CASANDRA_BRITO_GOMEZ <a.22308051280559@cbtis128.edu.mx>
2025-03-24 17:17:01 +0000

    hola2

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MiCont());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiCont extends StatelessWidget {
+  const MiCont({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 175, 215, 224),
+        appBar: AppBar(
+          title: Text(
+            'Casandra Brito Gomez Mat:22308051280559',
+            style: TextStyle(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: const Color.fromARGB(255, 133, 106, 207),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200, // Ancho del contenedor
+                height:
+                    200, // Alto del contenedor (aumentado para acomodar el ícono)
+                padding: EdgeInsets.all(20), // Padding interno
+                margin: EdgeInsets.all(10), // Margen externo
+                decoration: BoxDecoration(
+                  color:
+                      const Color.fromARGB(255, 212, 93, 206), // Color de fondo
+                  borderRadius: BorderRadius.circular(15), // Bordes redondeados
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 96, 134, 216)
+                          .withOpacity(0.7), // Sombra
+                      spreadRadius: 3,
+                      blurRadius: 5,
+                      offset: Offset(0, 7), // Desplazamiento de la sombra
+                    ),
+                  ],
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, // Centra el contenido verticalmente
+                  children: [
+                    Text(
+                      'Contenedor!!',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10), // Espacio entre el texto y el ícono
+                    Icon(
+                      Icons
+                          .auto_awesome_rounded, // Ícono de un gato (o mascota)
+                      color: Colors.white,
+                      size: 40, // Tamaño del ícono
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
